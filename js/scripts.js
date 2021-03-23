3307b9af406f360bbb417cd3628d3b7749fc0f0c
Hoon <sostar0832@cau.ac.kr>
2021-03-24 01:00:09 +0900

    21.03.24
    매모리피스 계산 기능완성
    퓨어메모리피스 계산 추가

--- js/scripts.js
+++ js/scripts.js
@@ -22,8 +22,8 @@ function pure_MP_calculator_hide() {
 }
 
 //하드 피스 케릭터 사진 + 계산기 표시
-function getHDCharacterView(){
-    var getID = document.getElementById("character-main-HD-list-select");
+function getPieceCharacterView(){
+    var getID = document.getElementById("character-MemoryPeiece-list-select");
     var stage = getID.options[getID.selectedIndex].value;
 
     document.getElementById("peko_HD_checkbox").style.display="none";
@@ -35,41 +35,41 @@ function getHDCharacterView(){
     document.getElementById("yukari_HD_checkbox").style.display="none";
 
     if(stage=="default_character"){
-        document.getElementById("preset-HD-character-image").src="priconne-images/unit-icon/Placeholder.webp" 
+        document.getElementById("preset-HD-character-image").src="priconne-images/unit-icon/Placeholder.webp";
         document.getElementById("preset-HD-character-name-label").innerText="";
     }
     if(stage=="VH18-1"){
-        document.getElementById("preset-HD-character-image").src="priconne-images/unit-icon/페코린느3성.webp" 
+        document.getElementById("preset-HD-character-image").src="priconne-images/unit-icon/페코린느3성.webp" ;
         document.getElementById("preset-HD-character-name-label").innerText="페코린느";
         document.getElementById("peko_HD_checkbox").style.display="block";
     }
     if(stage=="VH18-2"){
-        document.getElementById("preset-HD-character-image").src="priconne-images/unit-icon/콧코로3성.webp" 
+        document.getElementById("preset-HD-character-image").src="priconne-images/unit-icon/콧코로3성.webp" ;
         document.getElementById("preset-HD-character-name-label").innerText="콧코로";
         document.getElementById("koko_HD_checkbox").style.display="block";
     }
     if(stage=="VH18-3"){
-        document.getElementById("preset-HD-character-image").src="priconne-images/unit-icon/캬루3성.webp" 
+        document.getElementById("preset-HD-character-image").src="priconne-images/unit-icon/캬루3성.webp" ;
         document.getElementById("preset-HD-character-name-label").innerText="캬루";
         document.getElementById("karyl_HD_checkbox").style.display="block";
     }
     if(stage=="VH19-1"){
-        document.getElementById("preset-HD-character-image").src="priconne-images/unit-icon/리노3성.webp" 
+        document.getElementById("preset-HD-character-image").src="priconne-images/unit-icon/리노3성.webp" ;
         document.getElementById("preset-HD-character-name-label").innerText="리노";
         document.getElementById("rino_HD_checkbox").style.display="block";
     }
     if(stage=="VH19-2"){
-        document.getElementById("preset-HD-character-image").src="priconne-images/unit-icon/리마3성.webp" 
+        document.getElementById("preset-HD-character-image").src="priconne-images/unit-icon/리마3성.webp" ;
         document.getElementById("preset-HD-character-name-label").innerText="리마";
         document.getElementById("rima_HD_checkbox").style.display="block";
     }
     if(stage=="VH19-3"){
-        document.getElementById("preset-HD-character-image").src="priconne-images/unit-icon/이오3성.webp" 
+        document.getElementById("preset-HD-character-image").src="priconne-images/unit-icon/이오3성.webp" ;
         document.getElementById("preset-HD-character-name-label").innerText="이오";
         document.getElementById("io_HD_checkbox").style.display="block";
     }
     if(stage=="VH20-1"){
-        document.getElementById("preset-HD-character-image").src="priconne-images/unit-icon/유카리3성.webp" 
+        document.getElementById("preset-HD-character-image").src="priconne-images/unit-icon/유카리3성.webp"; 
         document.getElementById("preset-HD-character-name-label").innerText="유카리";
         document.getElementById("yukari_HD_checkbox").style.display="block";
     }
@@ -77,7 +77,48 @@ function getHDCharacterView(){
     location.href="#container-MP-calculator";
 }
 
-// 베리하드 케릭터 사진 + 계산기 표시
+//베리 하드 피스 케릭터 사진 + 계산기 표시
+function getPurePieceCharacterView(){
+    var getID = document.getElementById("character-PureMemoryPeiece-list-select");
+    var stage = getID.options[getID.selectedIndex].value;
+
+    if(stage=="default_character"){
+        document.getElementById("preset-pure-HD-character-image").src="priconne-images/unit-icon/Placeholder.webp"; 
+        document.getElementById("preset-pure-HD-character-name-label").innerText="";
+    }
+    if(stage=="VH18-1"){
+        document.getElementById("preset-pure-HD-character-image").src="priconne-images/unit-icon/페코린느3성.webp" ;
+        document.getElementById("preset-pure-HD-character-name-label").innerText="페코린느";
+    }
+    if(stage=="VH18-2"){
+        document.getElementById("preset-pure-HD-character-image").src="priconne-images/unit-icon/콧코로3성.webp" ;
+        document.getElementById("preset-pure-HD-character-name-label").innerText="콧코로";
+    }
+    if(stage=="VH18-3"){
+        document.getElementById("preset-pure-HD-character-image").src="priconne-images/unit-icon/캬루3성.webp" ;
+        document.getElementById("preset-pure-HD-character-name-label").innerText="캬루";
+    }
+    if(stage=="VH19-1"){
+        document.getElementById("preset-pure-HD-character-image").src="priconne-images/unit-icon/리노3성.webp" ;
+        document.getElementById("preset-pure-HD-character-name-label").innerText="리노";
+    }
+    if(stage=="VH19-2"){
+        document.getElementById("preset-pure-HD-character-image").src="priconne-images/unit-icon/리마3성.webp" ;
+        document.getElementById("preset-pure-HD-character-name-label").innerText="리마";
+    }
+    if(stage=="VH19-3"){
+        document.getElementById("preset-pure-HD-character-image").src="priconne-images/unit-icon/이오3성.webp" ;
+        document.getElementById("preset-pure-HD-character-name-label").innerText="이오";
+    }
+    if(stage=="VH20-1"){
+        document.getElementById("preset-pure-HD-character-image").src="priconne-images/unit-icon/유카리3성.webp" ;
+        document.getElementById("preset-pure-HD-character-name-label").innerText="유카리";
+    }
+
+    location.href="#container-pure-MP-calculator";
+}
+
+// 베리하드 케릭터 사진 + 공략글
 function getVHCharacterView() {
     var getID = document.getElementById("character-main-VH-list-select");
     var stage = getID.options[getID.selectedIndex].value;
@@ -134,10 +175,10 @@ function getVHCharacterView() {
 }
 
 // 하드 피스 계산기 버튼
-function calculateHDpiece(){
+function calculatePiece(){
     document.getElementById("MP-calculate-result").style.display="block";
 
-    var getID = document.getElementById("character-main-HD-list-select");
+    var getID = document.getElementById("character-MemoryPeiece-list-select");
     var stage = getID.options[getID.selectedIndex].value;
 
     if(stage=="VH18-1"){
@@ -210,5 +251,17 @@ function calculateHDpiece(){
         document.getElementById('MP-num').innerText = String(piecefive*5);
         document.getElementById('MP-jewelnum').innerText = String(0);
     }
+}
 
+function calculatePurePiece(){
+    document.getElementById("PMP-calculate-result").style.display="block";
+
+    var times = $('input:radio[name="pure-character-times"]:checked').val();
+    var isfull = $('input:checkbox[name="pure-character-reset"]').is(':checked');
+    var full = 1; if(isfull) full = 2;
+    var days = document.getElementById('pure-character-days').value;
+    document.getElementById('PMP-days').innerText = String(days);
+    document.getElementById('PMP-num').innerText = String(3*times*days/2);
+    document.getElementById('PMP-jewelnum').innerText = String(0);
+    if($('input:checkbox[name="pure-character-reset"]').is(':checked')) document.getElementById('PMP-jewelnum').innerText = String(50 * days);
 }
